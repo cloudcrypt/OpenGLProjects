8b80150fb43a854fdd37a4e4354079420ab3d218
Daniel D <cloudcrypt@users.noreply.github.com>
2017-11-25 03:25:58 -0700

    Removed extra shaders

--- 3DModels/data/tessellationControl.glsl
+++
@@ -1,9 +0,0 @@
-#version 410
-
-layout(vertices = 4) out;
-
-void main() {
-  gl_TessLevelOuter[0] = 1;
-  gl_TessLevelOuter[1] = 16;
-  gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
-}
